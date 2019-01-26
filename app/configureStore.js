c97f6e8159cd690e488f1a9cc8e3853824dd557c
Jeff Green <green.jeff.m@gmail.com>
2019-01-26 11:57:34 -0600

    Add redux-logger

--- app/configureStore.js
+++ app/configureStore.js
@@ -5,6 +5,7 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import { fromJS } from 'immutable';
 import { routerMiddleware } from 'connected-react-router/immutable';
+import logger from 'redux-logger';
 import createSagaMiddleware from 'redux-saga';
 import createReducer from './reducers';
 
@@ -14,7 +15,8 @@ export default function configureStore(initialState = {}, history) {
   // Create the store with two middlewares
   // 1. sagaMiddleware: Makes redux-sagas work
   // 2. routerMiddleware: Syncs the location/URL path to the state
-  const middlewares = [sagaMiddleware, routerMiddleware(history)];
+  // 3. logger: logs tool that lets you replay problems as if they happened in your own browser
+  const middlewares = [sagaMiddleware, routerMiddleware(history), logger];
 
   const enhancers = [applyMiddleware(...middlewares)];
 
