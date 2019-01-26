52c235a4063396afe135af9ab88b35dc02484f11
Jeff Green <green.jeff.m@gmail.com>
2019-01-26 12:22:14 -0600

    Add routing

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -7,19 +7,31 @@
  */
 
 import React from 'react';
-import { Helmet } from 'react-helmet';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, withRouter } from 'react-router-dom';
+import { Auth } from 'aws-amplify';
+import { connect } from 'react-redux';
+import { compose } from 'redux';
+import { createStructuredSelector } from 'reselect';
+import injectReducer from 'utils/injectReducer';
+import injectSaga from 'utils/injectSaga';
 
-import LoginPage from 'containers/LoginPage/Loadable';
-import SignUpPage from 'containers/SignUpPage/Loadable';
 import HomePage from 'containers/HomePage/Loadable';
 import FeaturePage from 'containers/FeaturePage/Loadable';
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
+import LoginPage from 'containers/LoginPage/Loadable';
+import SignUpPage from 'containers/SignUpPage/Loadable';
 import Header from 'components/Header';
 import Footer from 'components/Footer';
-
+import {
+  makeSelectIsAuthenticated,
+  makeSelectIsAuthenticating,
+} from '../LoginPage/selectors';
 import GlobalStyle from '../../global-styles';
+import { logout, userHasAuthenticated } from '../LoginPage/actions';
+import reducer from '../LoginPage/reducer';
+import saga from '../LoginPage/saga';
 
 const AppWrapper = styled.div`
   max-width: calc(768px + 16px * 2);
@@ -30,25 +42,74 @@ const AppWrapper = styled.div`
   flex-direction: column;
 `;
 
-export default function App() {
-  return (
-    <AppWrapper>
-      <Helmet
-        titleTemplate="%s - React.js Boilerplate"
-        defaultTitle="React.js Boilerplate"
-      >
-        <meta name="description" content="A React.js Boilerplate application" />
-      </Helmet>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/login" component={LoginPage} />
-        <Route path="/signup" component={SignUpPage} />
-        <Route path="/features" component={FeaturePage} />
-        <Route path="" component={NotFoundPage} />
-      </Switch>
-      <Footer />
-      <GlobalStyle />
-    </AppWrapper>
-  );
+class App extends React.PureComponent {
+  async componentDidMount() {
+    try {
+      await Auth.currentSession();
+      this.props.onUserHasAuthenticated(true);
+    } catch (e) {
+      if (e !== 'No current user') {
+        alert(e);
+      }
+    }
+  }
+
+  render() {
+    const { isAuthenticated, onLogout } = this.props;
+    const headerProps = {
+      isAuthenticated,
+      onLogout,
+    };
+
+    return (
+      <AppWrapper>
+        <Header {...headerProps} />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/features" component={FeaturePage} />
+          <Route path="/login" component={LoginPage} />
+          <Route path="/signup" component={SignUpPage} />
+          <Route path="" component={NotFoundPage} />
+        </Switch>
+        <Footer />
+        <GlobalStyle />
+      </AppWrapper>
+    );
+  }
 }
+
+App.propTypes = {
+  isAuthenticated: PropTypes.bool,
+  isAuthenticating: PropTypes.bool,
+  onLogout: PropTypes.func,
+  onUserHasAuthenticated: PropTypes.func,
+};
+
+export function mapDispatchToProps(dispatch) {
+  return {
+    onLogout: () => dispatch(logout()),
+    onUserHasAuthenticated: evt => {
+      dispatch(userHasAuthenticated(evt));
+    },
+  };
+}
+
+const mapStateToProps = createStructuredSelector({
+  isAuthenticated: makeSelectIsAuthenticated(),
+  isAuthenticating: makeSelectIsAuthenticating(),
+});
+
+const withConnect = connect(
+  mapStateToProps,
+  mapDispatchToProps,
+);
+
+const withReducer = injectReducer({ key: 'login', reducer });
+const withSaga = injectSaga({ key: 'login', saga });
+
+export default compose(
+  withRouter,
+  withReducer,
+  withSaga,
+  withConnect,
+)(App);
